8245a60ed3166cf78483eaee9bd25269d89ed22e
TonyYang <tonyhappystyle@163.com>
2020-12-02 06:56:17 +0000

    初始化

---
+++ about/overview.php
@@ -0,0 +1,125 @@
+﻿<?php 
+include $_SERVER['DOCUMENT_ROOT'].'/menu.php'; 
+include $_SERVER['DOCUMENT_ROOT'].'/headerWrapper.php'; 
+include $_SERVER['DOCUMENT_ROOT'].'/footerWrapper.php'; 
+
+?>
+<!doctype html>
+<html lang="zh-CN">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="SKYPE_TOOLBAR" content ="SKYPE_TOOLBAR_PARSER_COMPATIBLE"/>
+<link rel= "shortcut icon" media="all" type="image/x-icon" href="/favicon.ico" />
+<link rel= "icon" media="all" type="image/vnd.microsoft.icon" href="/favicon.ico" /><title>万家灯火科技</title>
+<link href="/css/jquery.swiper.css" rel="stylesheet" type="text/css">
+<link href="/css/easydropdown.css" rel="stylesheet" type="text/css">
+<link href="/css/magnific-popup.css" rel="stylesheet" type="text/css">
+<link href="/css/reset.css" rel="stylesheet" type="text/css">
+<link href="/css/style.css" rel="stylesheet" type="text/css">
+<!--[if lt IE 9]>
+    <link href="/css/style_1024.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+<link href="/css/style_1024.css" rel="stylesheet" type="text/css" media="screen and (min-width:960px)" />
+<link href="/css/style_768.css" rel="stylesheet" type="text/css" media="screen and (max-width:959px) and (min-width:768px)" />
+<link href="/css/style_320.css" rel="stylesheet" type="text/css" media="screen and (max-width:767px)" />
+
+
+<link href="/css/style_mobile_nav.css" rel="stylesheet" type="text/css" media="screen and (max-width:767px)" />
+
+
+<link href="/css/style_1024.css" rel="stylesheet" type="text/css" media="print" />
+<link href="/css/print.css" rel="stylesheet" type="text/css" media="print" />
+<script type="text/javascript" src="/js/jquery.min.js"></script>
+<script type="text/javascript" src="/js/jquery-ui.min.js"></script>
+<script src="/js/jquery.easydropdown.js"></script>
+<script src="/js/jquery.scrollto.js"></script>
+<script src="/js/jquery.cycle2.min.js"></script>
+<script src="/js/jquery.swiper-2.0.min.js"></script>
+<script src="/js/jquery.swiper.hashnav.min.js"></script>
+<script src="/js/jquery.appear.js"></script>
+<!--<script type="text/javascript" src="/js/addthis_widget.js"></script>-->
+<script src="/js/highcharts.js"></script>
+<script src="/js/exporting.js"></script>
+<script src="/js/grid.js"></script>
+<script src="/js/appear_animate.js"></script>
+<script src="/js/jquery.redirect.js"></script>
+<script src="/js/jquery.magnific-popup.min.js"></script>
+<script src="/js/common.js"></script>
+
+<style>
+	#overview p.remark{ font-size:75%; }
+</style>
+</head>
+<body><script>!(function(c,b,d,a){c[a]||(c[a]={});c[a].config={pid:"ggcvhz3u94@8f03de3e59271b7",appType:"web",imgUrl:"https://arms-retcode.aliyuncs.com/r.png?",enableSPA:true,useFmp:true,sendResource:true};with(b)with(body)with(insertBefore(createElement("script"),firstChild))setAttribute("crossorigin","",src=d)})(window,document,"https://retcode.alicdn.com/retcode/bl.js","__bl");</script>
+	<div class="headerWrapper">
+    <?=$headerWrapper?> 
+</div>
+<div class="nav">
+<?=$menu_content?>
+</div>
+<div class="wrapper"><div class="container" id="overview">
+	<h1 class="title">公司简介</h1>
+    <div class="mainWrapper">
+    	<div class="row clearfix" id="row1">
+            <div class="col">
+                <div class="img" id="img1"><img src="/picture/1.png" /></div>
+            </div>
+        	<div class="col">
+                <div class="text" id="text1">
+                	<h2>万家灯火科技的使命是构筑新时代的智慧城市。</h2>
+                    <p>我们旨在助力城市发展，帮助其变革交通、住宅和商业的运行方式，提升整个城市的运转效率。我们为公安、民政及其他企业提供技术基础设施以及数据平台，帮助其借助新技术的力量与城市居民进行互动，并更高效地进行管理。</p>
+                    <p>我们的业务包括社区信息采集、人员车辆的物联网智能管控、社区大数据深度挖掘及社区物业流程的数字化。除此之外， 我们在银行人脸识别、工厂数字管理、校园智慧管控、VR全景教学培训等业务有着新的扩展，主要客户是公安、银行、物业、工厂、学校。围绕着我们的平台与业务，一个涵盖了人脸识别、大数据分析、设备整合、物业智慧管理、第三方服务提供商、战略合作伙伴及其他企业的数字经济体已经建立。</p>
+					<p>截至2020年11月30日止，万家灯火科技已经为唐山迁安的建立了成熟的智慧社区网络，是唐山唯一的一家有能力与公安建立数据互联的企业。</p>
+					<p class="remark">* 为贯彻习近平总书记推动城市治理体系和治理能力现代化的智慧城市指导思想，全国多省公安厅印发了《全省公安机关加快推进智慧安防建设的工作方案》文件。</p>
+				</div>
+			</div>
+        </div>
+        <div class="row clearfix" id="row2">
+        	<div class="col r">
+            	<div class="img" id="img2"><img src="/picture/5.png" /></div>
+			</div>        
+        	<div class="col l">
+            	<div class="text" id="text2">
+            		<h2>我们的愿景</h2>
+					<p>我们不追求独大，不追求最强；我们追求成为一家让所有合作伙伴互利共赢的好公司。我们旨在构建未来的数字化基础设施。我们的愿景是让智慧城市成为新时代财富的源泉。</p>
+				</div>
+            	<div class="text" id="text3">
+            		<h2 class="orangeHeading">互利共赢：</h2>
+					<p>万家灯火科技创立于2020年，之前经过了6年的筹备，一经成立，立即成为京津冀地区技术最先进的智慧。我们的文化、商业模式和系统的建立都要经得起时间考验，让我们得以长期可持续发展。阿里巴巴的终极目标，就是为社会创造价值，更好地解决社会问题，变阿里巴巴的能力为中小企业发展的能力，为整个社会进步的动力。</p>
+				</div>
+                <div class="text" id="text4">
+            		<h2 class="orangeHeading">相会在阿里巴巴：</h2>
+					<p>我们助力数以亿计的用户之间、消费者与商家之间、各企业之间的日常商业和社交互动。</p>
+				</div>
+            	<div class="text" id="text5">
+            		<h2 class="orangeHeading">工作在阿里巴巴：</h2>
+					<p>我们向客户提供商业基础设施和新技术，让他们建立业务、创造价值，并与我们数字经济体的参与者共享收益。</p>
+				</div>
+            	<div class="text" id="text6">
+            		<h2 class="orangeHeading">生活在阿里巴巴：</h2>
+					<p>我们致力于拓展产品和服务范畴，让阿里巴巴成为我们客户日常生活的重要部份。<br /><br />
+					随着我们的业务不断扩展，从商业拓展至云计算、数字媒体及娱乐等众多其他领域，阿里巴巴已进化为一个独特的、充满活力与创新的数字经济体。我们已设立了<b>五年目标</b>，到2024财年底，我们的中国消费者业务将服务超过10亿的年度活跃消费者，实现超过人民币10万亿元的年度消费额，并基于此全面走向全球化。我们相信五年目标使我们能更接近实现<b>2036年的愿景</b>：服务全世界20亿消费者，帮助1,000万家中小企业盈利以及创造1亿就业机会。</p>
+				</div>
+			</div>
+        </div>
+    	<div class="row clearfix" id="row3">
+        	<div class="col">
+            	<div class="text" id="text7">
+            		<h2>阿里巴巴集团由曾担任英语教师的马云与其他来自不同背景的伙伴共18人，于1999年在中国杭州创立。<span class="note">我们的创始人创办本公司是为了支持小企业发展，他们相信互联网能够创造公平的环境，让小企业通过创新与科技拓展业务，并更有效地参与中国及国际市场竞争。我们坚信专注于客户需求并为其提供解决方案（不论客户是消费者、商家或是企业），最终将引领我们的业务走向成功。</span></h2>
+				</div>
+				<div class="img" id="img5"><img src="/picture/4.png" /></div>
+            </div>
+        	<div class="col">
+				<div class="img" id="img6"><img src="/picture/3.png" /></div>
+			</div>
+		</div>
+    </div>
+</div>
+<div class="push"></div>
+</div>
+<div class="footerWrapper">
+	<?=$footerWrapper?>
+</div>
+</body>
+</html>
